dd604e5759020754d3fad97ce1cf2cf69368c8fa
jiaxuan <49102292+HOSHINOANNA@users.noreply.github.com>
2021-01-16 20:07:24 +0000

    排序：maop排序，插入排序，选择排序

--- sorting/sort.py
+++ sorting/sort.py
@@ -25,10 +25,8 @@ def selectionSort(arr):
                 min_index = j
         arr[min_index], arr[i] = arr[i], arr[min_index]
     return arr
-arr = [3, 2, 0, 7, 4]
-result = selectionSort(arr)
 
-#将第一待排序序列第一个元素看做一个有序序列,把第二个元素到最后一个元素当成是未排序序列。
+# 将第一待排序序列第一个元素看做一个有序序列,把第二个元素到最后一个元素当成是未排序序列。
 
 # 从头到尾依次扫描未排序序列将扫描到的每个元素插入有序序列的适当位置。（
 # 如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
@@ -42,3 +40,4 @@ def intersectionSort(arr):
             arr[index] = arr[index - 1]  # 将之前排好序的元素向后移
             index -= 1
         arr[index] = key  # 以上将元素向后移后，将新加元素插入它的正确的位置上
+
